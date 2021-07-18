1ba9cad74c8f790e24966d5faaf838943f310b4c
dar90 <darrekk90@gmail.com>
2021-07-18 20:37:49 +0200

    add find by login and email methods

--- api/src/main/java/com/example/api/repository/UserRepository.java
+++ api/src/main/java/com/example/api/repository/UserRepository.java
@@ -1,9 +1,12 @@
 package com.example.api.repository;
 
+import java.util.Optional;
+
 import com.example.api.model.User;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-    
+    Optional<User> findByLogin(String login);
+    Optional<User> findByEmail(String email);
 }
