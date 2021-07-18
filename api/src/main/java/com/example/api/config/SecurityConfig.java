d7cf27d862e2a247c655d31ceadceb4aa1c00219
dar90 <darrekk90@gmail.com>
2021-07-18 20:36:32 +0200

    add password encoder and auth manager builder

--- api/src/main/java/com/example/api/config/SecurityConfig.java
+++ api/src/main/java/com/example/api/config/SecurityConfig.java
@@ -1,13 +1,26 @@
 package com.example.api.config;
 
+import com.example.api.repository.UserRepository;
+
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final UserRepository userRepository;
+
+    public SecurityConfig(UserRepository repository) {
+        userRepository = repository;
+    }
     
     // temporary config
     @Override
@@ -19,4 +32,18 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .cors().disable()
                 .csrf().disable();
     }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(
+            username -> userRepository.findByLogin(username)
+                                    .orElseThrow(() -> new UsernameNotFoundException(username + " not found"))
+        );
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
 }
