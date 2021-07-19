82284dc12f1f6ed8a2f29c6a65a7156cff068316
dar90 <darrekk90@gmail.com>
2021-07-19 12:46:13 +0200

    add RegistrationForm validation

--- api/src/main/java/com/example/api/dto/RegistrationForm.java
+++ api/src/main/java/com/example/api/dto/RegistrationForm.java
@@ -1,3 +1,10 @@
 package com.example.api.dto;
 
-public record RegistrationForm(String login, String email, String password, String repeatedPassword) {}
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+public record RegistrationForm(@NotBlank @Size(min = 4, max = 32) String login, 
+                                @NotBlank @Email String email, 
+                                @NotBlank @Size(min = 8, max = 50) String password, 
+                                @NotBlank @Size(min = 8, max = 50) String repeatedPassword) {}
