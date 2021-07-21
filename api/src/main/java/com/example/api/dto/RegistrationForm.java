8707c6c0e1b553400341d543045fdd6f7455c7c8
dar90 <darrekk90@gmail.com>
2021-07-21 12:43:04 +0200

    change required lengths for login and password

--- api/src/main/java/com/example/api/dto/RegistrationForm.java
+++ api/src/main/java/com/example/api/dto/RegistrationForm.java
@@ -4,7 +4,7 @@ import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
 
-public record RegistrationForm(@NotBlank @Size(min = 4, max = 32) String login, 
+public record RegistrationForm(@NotBlank @Size(min = 5, max = 25) String login, 
                                 @NotBlank @Email String email, 
-                                @NotBlank @Size(min = 8, max = 50) String password, 
-                                @NotBlank @Size(min = 8, max = 50) String repeatedPassword) {}
+                                @NotBlank @Size(min = 5, max = 55) String password, 
+                                @NotBlank @Size(min = 5, max = 55) String repeatedPassword) {}
