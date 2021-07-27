a23dc0fd5a26e6ace9dc43ecd6d620208f78253f
dar90 <darrekk90@gmail.com>
2021-07-27 15:42:08 +0200

    implement category and recipes-list

--- ui/src/app/category/category.component.ts
+++ ui/src/app/category/category.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Category} from "../_model/Category";
+import {HttpClient} from "@angular/common/http";
+import {AppSettings} from "../AppSettings";
+import {ActivatedRoute} from "@angular/router";
 
 @Component({
   selector: 'app-category',
@@ -7,9 +11,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CategoryComponent implements OnInit {
 
-  constructor() { }
+  category: Category;
+
+  constructor(private httpClient: HttpClient,
+              private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.route.params.subscribe(
+      params => {
+        this.httpClient.get<Category>(AppSettings.API_URL + '/category/' + params['name']).subscribe(
+          response => this.category = response
+        )
+      }
+    );
+  }
+
+  get recipes() {
+    return this.category?.recipes;
   }
 
 }
--- ui/src/app/recipes-list/recipes-list.component.ts
+++ ui/src/app/recipes-list/recipes-list.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
+import {Recipe} from "../_model/Recipe";
 
 @Component({
   selector: 'app-recipes-list',
@@ -6,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipes-list.component.scss']
 })
 export class RecipesListComponent implements OnInit {
+
+  @Input() recipesList: Recipe[];
 
   constructor() { }
 
