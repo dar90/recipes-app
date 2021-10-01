78a7d2ba0198504f67834c4123dac56aa213cb7c
dar90 <darrekk90@gmail.com>
2021-10-01 18:54:16 +0200

    add info about hosting and demo account

--- ui/src/app/homepage/homepage.component.ts
+++ ui/src/app/homepage/homepage.component.ts
@@ -3,6 +3,7 @@ import {Recipe} from "../_model/Recipe";
 import {HttpClient} from "@angular/common/http";
 import {AppSettings} from "../AppSettings";
 import {Category} from "../_model/Category";
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-homepage',
@@ -14,9 +15,18 @@ export class HomepageComponent implements OnInit {
   latestRecipes: Recipe[];
   categories: Category[];
 
-  constructor(private httpClient: HttpClient) { }
+  constructor(private httpClient: HttpClient, 
+              private snackBar: MatSnackBar) { }
 
   ngOnInit(): void {
+    if(!sessionStorage.getItem('showHostingAndAccountInfo')) {
+      this.snackBar.open('Ładowanie danych może zająć kilkanaście sekund, ponieważ strona korzysta z darmowego hostingu - prosimy o chwilę cierpliwości :)', 'OK', {duration: 10000});
+      this.snackBar._openedSnackBarRef?.afterDismissed().subscribe(
+        () => this.snackBar.open('Konto demonstracyjne - login: admin, hasło: admin', 'OK')
+      );
+    }
+    sessionStorage.setItem('showHostingAndAccountInfo', 'false');
+
     this.httpClient.get<Recipe[]>(AppSettings.API_URL + '/recipe').subscribe(
       response => {
         this.latestRecipes = response.sort((a, b) => b.id - a.id)
